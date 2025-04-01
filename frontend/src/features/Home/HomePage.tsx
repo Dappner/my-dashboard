d287daff42f10a4a4bd8eb67a589cbed905018dc
Dappner <nicklasastorian@googlemail.com>
2025-03-31 21:51:40 -0400

    Attempted to fix the Github Action

--- frontend/src/features/Home/HomePage.tsx
+++ frontend/src/features/Home/HomePage.tsx
@@ -1,9 +1,9 @@
 import KpiCard from "@/components/customs/KpiCard";
+import LoadingSpinner from "@/components/layout/components/LoadingSpinner";
 import { PageContainer } from "@/components/layout/components/PageContainer";
 import { calculatePortfolioMetrics } from "@/services/portfolioMetrics";
 import { TrendingUp } from "lucide-react";
-import { usePortfolioDailyMetrics } from "../Investing/hooks/usePortfolioDailyMetrics";
-import LoadingSpinner from "@/components/layout/components/LoadingSpinner";
+import { usePortfolioDailyMetrics } from "@/features/Investing/hooks/usePortfolioDailyMetrics";
 
 export default function HomePage() {
 	const { dailyMetrics, isLoading } = usePortfolioDailyMetrics("ALL");
@@ -14,15 +14,18 @@ export default function HomePage() {
 
 	return (
 		<PageContainer>
-			<KpiCard
-				title="Total Portfolio Value"
-				value={`$${metrics.currentTotalValue.toLocaleString(undefined, {
-					maximumFractionDigits: 2,
-				})}`}
-				changePercent={metrics.periodTotalChangePercent}
-				icon={TrendingUp}
-				positiveChange={metrics.periodInvestmentChange >= 0}
-			/>
+			<div className="grid sm:grid-cols-2 gap-4">
+				<KpiCard
+					title="Total Portfolio Value"
+					value={`$${metrics.currentTotalValue.toLocaleString(undefined, {
+						maximumFractionDigits: 2,
+					})}`}
+					changePercent={metrics.periodTotalChangePercent}
+					icon={TrendingUp}
+					positiveChange={metrics.periodInvestmentChange >= 0}
+				/>
+				<div>Spending Chart</div>
+			</div>
 		</PageContainer>
 	);
 }
