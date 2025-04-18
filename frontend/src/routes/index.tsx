9fa7d1edb3ad942303e5eb9386ea60e6d2d646b0
Dappner <nicklasastorian@googlemail.com>
2025-04-18 13:49:52 -0400

    Added back account route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,72 +6,72 @@ import LoginPage from "@/features/Auth/Login";
 import HabitsPage from "@/features/Habits/HabitsPage";
 import HomePage from "@/features/Home/HomePage";
 import {
-  createRootRoute,
-  createRoute,
-  createRouter,
-  useNavigate,
+	createRootRoute,
+	createRoute,
+	createRouter,
+	useNavigate,
 } from "@tanstack/react-router";
 import { investingRoutes } from "./investing-routes";
 import { readingRoutes } from "./reading-routes";
 import { spendingRoutes } from "./spending-routes";
 
 export const rootRoute = createRootRoute({
-  component: AuthWrapper,
-  notFoundComponent: NotFoundComponent,
+	component: AuthWrapper,
+	notFoundComponent: NotFoundComponent,
 });
 
 export const loginRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "login",
-  component: LoginPage,
+	getParentRoute: () => rootRoute,
+	path: "login",
+	component: LoginPage,
 });
 
 export const layoutRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/",
-  component: Layout,
-  notFoundComponent: NotFoundComponent,
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: Layout,
+	notFoundComponent: NotFoundComponent,
 });
 
 export const homeRoute = createRoute({
-  path: "home",
-  getParentRoute: () => layoutRoute,
-  component: HomePage,
+	path: "home",
+	getParentRoute: () => layoutRoute,
+	component: HomePage,
 });
 
 export const habitsRoute = createRoute({
-  getParentRoute: () => layoutRoute,
-  path: "habits",
-  component: HabitsPage,
+	getParentRoute: () => layoutRoute,
+	path: "habits",
+	component: HabitsPage,
 });
 
 export const accountRoute = createRoute({
-  getParentRoute: () => layoutRoute,
-  path: "account",
-  component: AccountPage,
+	getParentRoute: () => layoutRoute,
+	path: "account",
+	component: AccountPage,
 });
 
 export const splatRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "$",
-  component: () => {
-    const navigate = useNavigate();
-    navigate({ to: "/home" });
-    return null;
-  },
+	getParentRoute: () => rootRoute,
+	path: "$",
+	component: () => {
+		const navigate = useNavigate();
+		navigate({ to: "/home" });
+		return null;
+	},
 });
 
 const routeTree = rootRoute.addChildren([
-  loginRoute,
-  layoutRoute.addChildren([
-    homeRoute,
-    ...investingRoutes,
-    ...spendingRoutes,
-    ...readingRoutes,
-    accountRoute,
-    habitsRoute,
-    splatRoute,
-  ]),
+	loginRoute,
+	layoutRoute.addChildren([
+		homeRoute,
+		...investingRoutes,
+		...spendingRoutes,
+		...readingRoutes,
+		accountRoute,
+		habitsRoute,
+		splatRoute,
+	]),
 ]);
 
 export const router = createRouter({ routeTree });
--- frontend/src/routes/reading-routes.tsx
+++ frontend/src/routes/reading-routes.tsx
@@ -5,32 +5,32 @@ import { createRoute } from "@tanstack/react-router";
 import { layoutRoute } from ".";
 
 export const readingRoute = createRoute({
-  getParentRoute: () => layoutRoute,
-  path: "reading",
+	getParentRoute: () => layoutRoute,
+	path: "reading",
 });
 
 export const readingDashboardRoute = createRoute({
-  getParentRoute: () => readingRoute,
-  path: "/",
-  component: ReadingPage,
+	getParentRoute: () => readingRoute,
+	path: "/",
+	component: ReadingPage,
 });
 
 export const bookDetailRoute = createRoute({
-  getParentRoute: () => readingRoute,
-  path: "books/$bookId",
-  component: BookDetailPage,
+	getParentRoute: () => readingRoute,
+	path: "books/$bookId",
+	component: BookDetailPage,
 });
 
 export const authorDetailRoute = createRoute({
-  getParentRoute: () => readingRoute,
-  path: "authors/$authorId",
-  component: AuthorDetailPage,
+	getParentRoute: () => readingRoute,
+	path: "authors/$authorId",
+	component: AuthorDetailPage,
 });
 
 export const readingRoutes = [
-  readingRoute.addChildren([
-    readingDashboardRoute,
-    bookDetailRoute,
-    authorDetailRoute,
-  ]),
+	readingRoute.addChildren([
+		readingDashboardRoute,
+		bookDetailRoute,
+		authorDetailRoute,
+	]),
 ];
